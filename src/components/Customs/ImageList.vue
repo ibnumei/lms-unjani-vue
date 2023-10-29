6d431bad6c73d3eda6bb3f3b8b3a9ccc82892c0d
Arif Setyo Aji <arifsetyo.a1@gmail.com>
2023-10-29 17:16:07 +0700

    wip

---
+++ src/components/Customs/ImageList.vue
@@ -0,0 +1,54 @@
+<template>
+  <!-- <b-card @click.prevent="toggleItem($event,data.id)" :class="{'active' : selectedItems.includes(data.id)}" no-body>
+    <div class="">
+        <router-link :to="`?p=${data.id}`" class="w-40 position-relative">
+            <img :src="data.img" class="card-img-top" :alt="data.title" />
+        </router-link>
+    </div>
+    <b-card-body>
+        <b-row>
+            <b-colxx xxs="2">
+                <b-form-checkbox :checked="selectedItems.includes(data.id)" class="itemCheck mb-0" />
+            </b-colxx>
+            <b-colxx xxs="10" class="mb-3">
+                <h6 class="mb-4 card-subtitle">{{data.title}}</h6>
+                <p class="text-muted text-small mb-0 font-weight-light card-text">{{data.date}}</p>
+            </b-colxx>
+        </b-row>
+    </b-card-body>
+</b-card> -->
+  <div>
+    <div class="glide" ref="carouselImages">
+      <div data-glide-el="track" class="glide__track">
+        <div class="glide__slides">
+          <div
+              class="card"
+            >
+              <b-card class="flex-row" no-body>
+                <div class="w-100 position-relative">
+                  <img
+                    class="card-img"
+                    style="max-height: 210px"
+                    :src="data.img"
+                    alt="Card cap"
+                  />
+                </div>
+              </b-card>
+            </div>
+            <!-- <img :src="data.img" class="card-img-top" :alt="data.title" /> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["data", "selectedItems"],
+  methods: {
+    toggleItem(event, itemId) {
+      this.$emit("toggle-item", event, itemId);
+    },
+  },
+};
+</script>
---
+++ src/components/Customs/Scanner.vue
@@ -0,0 +1,60 @@
+<template>
+  <b-colxx
+    id="bColxx"
+    :xxs="12"
+    :sm="12"
+    :md="12"
+    :lg="12"
+    v-if="12"
+  >
+    <v-quagga
+      :onDetected="onBarcodeScanned"
+      :readerSize="readerSize"
+      :readerTypes="detecteds"
+      :patchSize="'medium'"
+    ></v-quagga>
+  </b-colxx>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      readerSize: {
+        width: 100,
+        height: 480,
+      },
+      detecteds: [
+        "code_128_reader",
+        "ean_reader",
+        "ean_8_reader",
+        "code_39_reader",
+        "code_39_vin_reader",
+        "codabar_reader",
+        "upc_reader",
+        "upc_e_reader",
+        "i2of5_reader",
+        "2of5_reader",
+        "code_93_reader",
+      ],
+      result: "",
+      format: "",
+    }
+  },
+  methods: {
+    onBarcodeScanned(barcode) {
+      console.log("detected", barcode);
+      this.result = barcode.codeResult.code;
+      this.format = barcode.codeResult.format;
+      // do something...
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+  .viewport canvas{
+    width: 30px !important
+  }
+  .viewport video{
+    width: 30px !important
+  }
+</style>
